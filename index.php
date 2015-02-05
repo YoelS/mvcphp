e174310c9d8683e4660f0d987749458538f23bec
Ekalaya Manullang <ekalaya@mugi.or.id>
2015-02-05 23:58:22 +0700

    Update LAE Framework
    okeh lae update lagi..

--- index.php
+++ index.php
@@ -1,13 +1,46 @@
-<?php 
-
-error_reporting(E_ALL); /* untuk menghenadle error jika ingin menghiden error ganti menjadi error_reporting(E_ERROR); */
-
-/* Seting pemisah direktori/folder pada unix untuk konsistensi  */
+<?php
+error_reporting(E_ALL);
+ 
+/* Nama folder aplikasi */
+$aplikasi_folder = '';
+ 
+/* Ganti pemisah direktori pada unix untuk konsistensi  */
 define('ROOT', str_replace("\\", "/", realpath(dirname(__FILE__))) . '/');
-
-	include_once("controller/Controller.php");
-
-	$controller = new Controller();
-	$controller->panggilclass();
-
-?>
+ 
+/* Menentukan BASEPATH sebagai root aplikasi */
+define('BASEPATH', ROOT . $aplikasi_folder . '/');
+ 
+/* Awal output buffering */
+ob_start();
+session_start();
+ 
+/* Menjalankan program melalui router */
+require BASEPATH . 'core/router.php';
+define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
+$router = new Router();
+$router->do_request();
+ 
+/* Akhir output buffering */
+@ob_end_flush();
+ 
+/* Tampilan Error */
+function show_error($message = '') {
+  ob_end_clean();
+  $error = '<html><head><title>Error</title>';
+  $error .= '<style type="text/css">';
+  $error .= 'body {margin:0; padding:0; font-family: sans-serif; color:#222;}';
+  $error .= '#error {margin: 30px auto; width: 600px; '.
+      ' border: 2px crimson solid; padding: 10px; '.
+      ' background: pink; text-align: center;}';
+  $error .= '</style>';
+  $error .= '</head><body><div id="error">';
+  if($message == '') {
+    $message = '<b>404 - Page not found!</b>';
+  }
+  $error .= $message;
+  $error .= '</div></body></html>';
+  exit ($error);
+}
+ 
+/* Akhir file index.php */
+/* Lokasi: ./index.php */
